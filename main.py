c8a635770552e435e95b005bd84b454225c0b410
AlwaysFr3sh <shimtucker22187@hotmail.com>
2020-12-20 21:38:25 +1100

    trained model, works decently ~7 mse if I calculated correctly

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ import numpy as np
 import cv2
 import time
 from tqdm import tqdm
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from utils import SpeedNet
+from utils import downscale
 
 # other people did this differently, whats that about? :(
 def dense_optical_flow(prvs, frame, mask):
-  flow = cv2.calcOpticalFlowFarneback(prvs,frame, None, 0.5, 3, 15, 3, 5, 1.5, 0) # I need to read about how this works lol
+  flow = cv2.calcOpticalFlowFarneback(prvs,frame, None, 0.5, 3, 15, 3, 5, 1.5, 0) 
   magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[...,1])
 
   mask[...,0] = angle * 180 / np.pi / 2 # no clue what this means too
@@ -17,63 +23,71 @@ def dense_optical_flow(prvs, frame, mask):
 
   return rgb 
 
-# code from main() is repeated here, it would be cool if I were to fix this TODO
-def process_vid(vid_location, labels_location):
-  cap = cv2.VideoCapture(vid_location)
-  labels = open(labels_location, "r")
-  processed = [] # kind of
+def get_predicted_speed(net, image):
+  with torch.no_grad():
+    net_out = net(image.view(-1, 1, 240, 320))
+  return float(net_out[0][0])
 
-  # get first frame for optical flow
-  ret, first_frame = cap.read()
-  prev_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
-  mask = np.zeros_like(first_frame)
-  mask[..., 1] = 255 # no clue what the mask is for (possibly to draw onto?)
-  
-  # processed for ML (hopefully)
-  for i in tqdm(range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))):
-    try:
-      ret, frame = cap.read()
-      gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      rgb = dense_optical_flow(prev_frame, gray_frame, mask)   
-      rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY) # this is really not that efficient
-      processed.append([np.array(rgb), labels.readline()])
-    except:
-      print("something went wrong")
+# more repeated code, which is bad, TODO: fix
+def average(lst):
+  return sum(lst) / len(lst)
 
-  # close stuff
-  cap.release() # cv VideoCapture  
-  labels.close() # file
-  return processed
+# returns grayscale image
+def grayscale(frame):
+  return cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
 def main():
   # get rid of the dumb UI elements we don't need
   cv2.namedWindow('input', cv2.WINDOW_GUI_NORMAL) # removes the weird buttons
-  cv2.resizeWindow("input", 1280, 480) # resize window
+  cv2.resizeWindow("input", 640, 480) # resize window
   cv2.moveWindow('input', -1500, 300) 
   cap = cv2.VideoCapture("data/train.mp4")
 
   # get first frame for optical flow
   ret, first_frame = cap.read()
-  prev_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+  first_frame = downscale(first_frame, 50)
+  prev_frame = grayscale(first_frame) 
   mask = np.zeros_like(first_frame)
   mask[..., 1] = 255 # no clue what the mask is for (possibly to draw onto?)
+
+  # get speed labels
+  # TODO: make sure speed labels align properly in training
+  # I would do this by checking that every frame has a corresponding speed, and the last frame speed
+  # isn't empty (I'm sure its fine)
+  real_speeds = open("data/train.txt", 'r')
+  net = SpeedNet()
+  net.load_state_dict(torch.load("models/big_data_model.pth"))
+  acc = [] # accumulated speed errors for mse
 
   # main loop
   while (cap.isOpened()):
     ret, frame = cap.read()
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = grayscale(downscale(frame, 50))
     rgb = dense_optical_flow(prev_frame, gray_frame, mask)
+    rgb = grayscale(rgb)
+    rgb = torch.Tensor(rgb)
+
+    # write speeds onto the image
+    speed = real_speeds.readline()[:-1] #chop whatever the unknown traling char is
+    pred_speed = get_predicted_speed(net, rgb)
+    acc.append((float(speed) - pred_speed)**2)
+
+    # write stats to frame before display
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, speed ,(0,30), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, str(pred_speed), (0, 60), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, str(average(acc)), (0, 90), font, 1, (255, 255, 0), 2, cv2.LINE_AA)
     
-    # join the two imgs to be fancy :)
-    img_concate = np.concatenate((frame, rgb),axis=1)
-    cv2.imshow('input', img_concate)
-    
+    # display
+    cv2.imshow('input', frame)
+
     prev_frame = gray_frame
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
       exit()
       break
 
+  print("MSE", average(acc))
   cap.release()
   cv2.destroyAllWindows()
 
